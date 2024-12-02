adc7d526d4abdbce313fdc52dbf1470efc39027d
Luivi23 <127993977+Luivi23@users.noreply.github.com>
2024-12-01 21:12:10 -0600

    bloque 2
    base de datos y formularios de usuario

---
+++ academica/academica/FrmInicio.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace academica
+{
+    public partial class FrmInicio : Form
+    {
+     
+        private void btnCancelar_Click_1(object sender, EventArgs e)
+        {
+            if (txtUsuario.Text == "usuario" && txtClave.Text == "clave")
+            {
+                principal principal = new principal();
+                this.Hide();
+                principal.Show();
+
+            }
+        }
+
+        private void btnIngresar_Click_1(object sender, EventArgs e)
+        {
+            if (txtUsuario.Text == "usuario" && txtClave.Text == "clave")
+            {
+                principal principal = new principal();
+                this.Hide();
+                principal.Show();
+
+            }
+            else
+            {
+                MessageBox.Show("los datos ingresados son incorrectos");
+            }
+        }
+    }
+}
+
+
+            
+       
+   
+
+
+
---
+++ academica/academica/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace academica {
+    internal static class Program {
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        [STAThread]
+        static void Main() {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new principal());
+        }
+    }
+}
+
+
+namespace academica
+{
+    public partial class FrmInicio : Form
+    {
+        public FrmInicio()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            txtUsuario.Clear();
+            txtClave.Clear();
+        }
+
+        private void btnIngresar_Click(object sender, EventArgs e)
+        {
+            if (txtUsuario.Text == "usuario" && txtClave.Text == "clave")
+            {
+                principal principal = new principal();
+                this.Hide();
+                principal.Show();
+
+            }
+            else
+            {
+                MessageBox.Show("los datos ingresados son incorrectos");
+            }
+        }
+
+    }
+}
+
+
+        
